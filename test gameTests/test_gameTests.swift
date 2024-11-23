f8475c601ffe9c494268862f2aa150511280f62b
ETHAN ZHOU <2263984904@qq.com>
2024-11-22 23:12:50 -0800

    Initial Commit

---
+++ test gameTests/test_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_gameTests.swift
+//  test gameTests
+//
+//  Created by 周梓洋 on 2024/11/22.
+//
+
+import Testing
+@testable import test_game
+
+struct test_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
